6a191d195d204a0d4c60c9492fe8b9e38bb5b855
pkll1g19 <pkll1g19@soton.ac.uk>
2022-02-25 09:54:21 +0000

    Implementation and tests for the distorter. Several bugs fixed

---
+++ src/3DReconstruction/distorter.py
@@ -0,0 +1,145 @@
+import math
+from msilib.schema import Error
+from multiprocessing.sharedctypes import Value
+import numpy as np
+
+class Distorter:
+    """ Given a pixel's coordinates, calculate the distortion based on the
+    height and width of the input image. """
+
+    def __init__(self, width, height, conv_size):
+        ''' Stores the height and width of the input image and the size of
+        the convolution kernel.'''
+
+        # try:
+        if width <= 0 or height <= 0 or conv_size < 0:
+            raise LessThanZeroException
+        self.width = width
+        self.height = height
+
+        if conv_size > width or conv_size > height:
+            raise TooBigKernelException
+
+        self.kernel_size = conv_size
+        self.R = self.relative_sampling_positions(conv_size)
+        # except TooBigKernelException:
+        #     print("The size of the kernel must not be larger than the image.")
+        # except LessThanZeroException:
+        #     print("The widths and the heights of the image and the kernel can't be zero.")
+
+    def relative_sampling_positions(self, size):
+        """ Computes the relative sampling positions as a matrix given the
+        required size of the square convolution kernel. Assumes that the size
+        of the kernel is odd so that there is a center. """
+
+        try:
+            if size % 2 == 0 or size < 1:
+                raise ValueError()
+
+            upper_lim = size // 2
+            lower_lim = -1 * upper_lim
+
+            return np.array([[(i, j) for j in range(lower_lim, upper_lim + 1)]
+                             for i in range(lower_lim, upper_lim + 1)])
+        except OddNumbersOnlyException:
+            print("Invalid size: {size}. The size must be a positive odd number.")
+
+    def lon_and_lat_2d(self, x, y):
+        """ Returns the latitude and longitude of given coordinates. """
+        lon = (x -(self.width / 2)) * ((2 * math.pi) / self.width)
+        lat = (self.height*0.5 - y) * (math.pi / self.height)
+        return lon, lat
+
+
+    def lon_and_lat_3d(self, x, y, z):
+        """ Backprojects, obtaining a latitude and longitude from the
+        spherical domain. """
+        lon = 0
+        if x == 0:
+            lon = math.pi / 2
+        elif x > 0:
+            lon = math.atan(z / x)
+        else:
+            lon = math.atan(z / x) + math.pi
+
+        lat = math.asin(y)
+        return lon, lat
+
+    def spherical_coords(self, lon, lat):
+        """ Returns the spherical coordinates of a given latitude and
+        longitude. """
+        xu = math.cos(lat) * math.sin(lon)
+        yu = math.sin(lat)
+        zu = math.cos(lat) * math.cos(lon)
+
+        return np.array([xu, yu, zu])
+
+    def tangent_plane_vectors(self, spherical_coords):
+        """ Computes the direction vectors that define a tangent plane to the
+        given spherical point, tx and ty. spherical_coords is a numpy array. """
+        spherical_coords = np.array(spherical_coords)
+        tx = self.normalize(np.cross(np.array([0, 1, 0]), spherical_coords))
+        ty = self.normalize(np.cross(spherical_coords, tx))
+        return tx, ty
+
+    def normalize(self, vector):
+        """ Takes in a vector and converts it into a unit vector with the same
+        direction. """
+        norm=np.linalg.norm(vector, ord=2)
+        if norm==0:
+            norm=np.finfo(vector.dtype).eps
+        return vector/norm
+
+
+    def sampling_grid(self, tx, ty, spherical_coords):
+        """ Computes the sampling grid on the tangent plane. """
+
+        # Spatial resolution - distance between elements
+        res = math.tan((2 * math.pi) / self.width)
+
+        # Computes the sampling grid locations.
+        sampling_grid_locations = np.empty(shape=(self.R.shape[0],
+                                                  self.R.shape[1], 3))
+        for i in range(0, self.R.shape[0]):
+            for j in range(0, self.R.shape[1]):
+                r = self.R[i][j]
+                tx = np.array(tx)
+                ty = np.array(ty)
+                sampling_grid_locations[i, j] = spherical_coords + (res * (tx * r[0] + ty * r[1]))
+        return sampling_grid_locations
+
+    def backpropogate(self, sampling_grid_locations):
+        """ Backpropogates point locations from the sampling grid to the
+        equirectangular domain. """
+
+        equirect = np.zeros(shape=(sampling_grid_locations.shape[0],
+                                   sampling_grid_locations.shape[1], 2))
+        for i in range(0, sampling_grid_locations.shape[0]):
+            for j in range(0, sampling_grid_locations.shape[1]):
+                point = sampling_grid_locations[i][j]
+                lon, lat = self.lon_and_lat_3d(point[0], point[1], point[2])
+                new_x = ((lon / (2 * math.pi)) + 0.5) * self.width
+                new_y = (0.5 - (lat / math.pi)) * self.height
+            equirect[i][j] = [new_x, new_y]
+
+        return equirect
+
+    def distort(self, x, y):
+        """ Distorts the sampling locations on the equirectangular plane. """
+        lon, lat = self.lon_and_lat_2d(x, y)
+        sphere_coords = self.spherical_coords(lon, lat)
+        tx, ty = self.tangent_plane_vectors(sphere_coords)
+        sampling_grid = self.sampling_grid(tx, ty, sphere_coords)
+        return self.backpropogate(sampling_grid)
+
+
+
+class TooBigKernelException(Exception):
+    pass
+
+class LessThanZeroException(Exception):
+    pass
+
+class OddNumbersOnlyException(Exception):
+    pass
+
---
+++ src/3DReconstruction/test_distorter.py
@@ -0,0 +1,88 @@
+import math
+import unittest
+from distorter import Distorter
+from numpy import testing
+
+class TestDistorter(unittest.TestCase):
+    def test_lon_and_lat_2d(self):
+        # Simple cases
+        simple_distorter = Distorter(200, 200, 3)
+        testing.assert_almost_equal(simple_distorter.lon_and_lat_2d(0, 0), (-1*math.pi, math.pi*0.5))
+        testing.assert_almost_equal(simple_distorter.lon_and_lat_2d(200, 200), (math.pi, math.pi*-0.5))
+        testing.assert_almost_equal(simple_distorter.lon_and_lat_2d(100, 100), (0, 0))
+
+        # More complex cases
+        testing.assert_almost_equal(simple_distorter.lon_and_lat_2d(159, 32), ((118*math.pi)/200, (17/50)*math.pi))
+
+        # Case with larger images
+        large_distorter = Distorter(1778, 3687, 3)
+        testing.assert_almost_equal(large_distorter.lon_and_lat_2d(20,1778), (-3.070915654, 0.05581077266))
+        testing.assert_almost_equal(large_distorter.lon_and_lat_2d(1778, 3687), (math.pi, -0.5*math.pi))
+
+        # Case with smaller images
+        small_distorter = Distorter(10, 5, 3)
+        testing.assert_almost_equal(small_distorter.lon_and_lat_2d(2, 1), (-0.6*math.pi, 0.3*math.pi))
+
+    def test_lon_and_lat_3d(self):
+        distorter = Distorter(200, 200, 3)
+        # Where x is less than 0
+        testing.assert_almost_equal(distorter.lon_and_lat_3d(-0.8830869300, 0.1722328441, 0.4387309164),
+                                    (2.6804962137, 0.1730959393))
+        # Where x is greater than 0
+        testing.assert_almost_equal(distorter.lon_and_lat_3d(1, 0, 0), (0, 0))
+
+        # Where x is 0
+        testing.assert_almost_equal(distorter.lon_and_lat_3d(0, 1, 0), (math.pi / 2, math.pi / 2))
+
+    def test_spherical_coords(self):
+        distorter = Distorter(200, 200, 3)
+
+        # Base cases
+        testing.assert_almost_equal(distorter.spherical_coords(-1*math.pi, 0.5*math.pi),
+                                (-7.50494368282489E-33,1,-6.1257422745431E-17))
+        testing.assert_almost_equal(distorter.spherical_coords(math.pi, -0.5*math.pi),
+                                (7.50494368282489E-33,-1,-6.1257422745431E-17))
+        testing.assert_almost_equal(distorter.spherical_coords(0, 0), (0,0,1))
+
+        # More complex cases
+        testing.assert_almost_equal(distorter.spherical_coords(2, 3), (-0.90019763, 0.141120008, 0.411982246))
+        testing.assert_almost_equal(distorter.spherical_coords(2.10424235, -1.034345098),
+                                    (0.440078184, -0.859527828, -0.259890564))
+        testing.assert_almost_equal(distorter.spherical_coords(0.0012, -0.0654),
+                                    (0.001197434, -0.065353389, 0.997861464))
+
+    def test_tangent_plane_vectors(self):
+        distorter = Distorter(200, 200, 3)
+
+        testing.assert_almost_equal(distorter.tangent_plane_vectors((-0.90019763, 0.141120008, 0.411982246)),
+                                    ((0.4161468367, 0, 0.9092974267), (0.1283244670, 0.9900264961, -0.0581409345)), 3)
+        print(distorter.tangent_plane_vectors((-0.90019763, 0.141120008, 0.411982246)))
+        testing.assert_almost_equal(distorter.tangent_plane_vectors((0.4400781840, -0.8595278280, -0.2598905640)),
+                                    ((-0.508729312126641, 0, -0.860926528214316), (0.740114726674641, 0.511079223419921, -0.437067750752457)), 3)
+        print(distorter.tangent_plane_vectors((0.4400781840, -0.8595278280, -0.2598905640)))
+        testing.assert_almost_equal(distorter.tangent_plane_vectors((0.0011974340, -0.0653533890, 0.9978614640)),
+                                    ((0.9999992800, 0, -0.0011999994), (0.0000784247, 0.9978712838, 0.0652142232)), 3)
+        print(distorter.tangent_plane_vectors((0.0011974340, -0.0653533890, 0.9978614640)))
+
+    def test_sampling_grid(self):
+        distorter = Distorter(200, 200, 3)
+        testing.assert_almost_equal(distorter.sampling_grid((0.4161468367, 0, 0.9092974267), (0.1283244670, 0.9900264961, -0.0581409345),
+                                                            (-0.90019763, 0.141120008, 0.411982246)),
+                                    [[[-0.917308330043916,0.110007171944807,0.385233575630315],
+                                     [-0.913275571204027,0.141120008,0.383406423154603],
+                                     [-0.909242812364138,0.172232844055192,0.381579270678892]],
+                                     [[-0.904230388839889,0.110007171944807,0.413809398475712],
+                                      [-0.90019763,0.141120008,0.411982246],
+                                      [-0.896164871160111,0.172232844055192,0.410155093524288]],
+                                     [[-0.891152447635862,0.110007171944807,0.442385221321108],
+                                      [-0.887119688795973,0.141120008,0.440558068845397],
+                                      [-0.883086929956084,0.172232844055192,0.438730916369685]]])
+    def test_backpropogate(self):
+        pass
+
+    def test_distort(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
